ad7409303c011fcfd77a22720497e333dd58a6af
finnbassill <tun39698@temple.edu>
2023-10-25 15:50:34 -0400

    Refactored for fragment use.

---
+++ app/src/main/java/edu/temple/fragmentrefactor/BlankFragment.kt
@@ -0,0 +1,38 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class BlankFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_blank, container, false)
+
+        val displayTextView = root.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = root.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = root.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Finnegan Bassill"
+            }
+        }
+        // Inflate the layout for this fragment
+        return root
+    }
+
+}
